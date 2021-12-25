3d00ed2db6963e26acace88db8148b3756036cb7
sophia <sophia@kahlan.org>
2021-12-24 21:26:02 -0800

    initial

---
+++ web-hbs/routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('client', { title: 'client' });
+});
+
+router.get('/client', function(req, res, next) {
+
+  res.render('client', { title: 'client' });
+});
+
+router.get('/server', function(req, res, next) {
+
+  res.render('server', { title: 'server' });
+});
+
+io.sockets.on('connection', function(socket) {
+  socket.on('play_sound', function(sound) {
+    io.emit('play_sound', sound);
+  });
+});
+
+module.exports = router;
